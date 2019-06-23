40d87dabcaee95d29da55098e9dc0b5fceb5a8ec
nelson <srandrediaz@gmail.com>
2019-06-23 21:16:01 +0200

    first unit testing project

---
+++ udemy.unittesting.spring.boot.projectzero/src/test/java/com/java/udemy/unittesting/spring/boot/episode/zero/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.udemy.unittesting.spring.boot.episode.zero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

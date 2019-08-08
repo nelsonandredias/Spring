55713fffad1a1b8698ca0686c1003f116ecaf21b
nelson <srandrediaz@gmail.com>
2019-08-08 10:21:15 +0100

    added example project with @profile annotation

---
+++ SpringFramework5/springframework.intro.springboot.di.profiles/src/main/java/springframework/intro/springboot/profiles/services/GreetingService.java
@@ -0,0 +1,7 @@
+package springframework.intro.springboot.profiles.services;
+
+public interface GreetingService {
+
+	public String sayGreeting();
+	
+}
---
+++ SpringFramework5/springframework.intro.springboot.di.profiles/src/test/java/springframework/intro/springboot/di/controllers/ConstructorInjectedControllerTest.java
@@ -0,0 +1,27 @@
+package springframework.intro.springboot.di.controllers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import springframework.intro.springboot.profiles.controllers.ConstructorInjectedController;
+import springframework.intro.springboot.profiles.services.GreetingServiceImpl;
+
+public class ConstructorInjectedControllerTest {
+
+	private ConstructorInjectedController constructorInjectedController;
+	
+	@Before
+	public void setUp() {
+		this.constructorInjectedController = new ConstructorInjectedController(new GreetingServiceImpl());
+	}
+	
+	@Test
+	public void testGreeting() {
+
+		//verify the output string in the controller is the same retrieving by the service implementation 
+		assertEquals(GreetingServiceImpl.HELLO_WORLD, constructorInjectedController.sayHello());
+	}
+	
+}

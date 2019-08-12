93bda8f976f5e4aed51e96bce8e964db3cf9b517
nelson <srandrediaz@gmail.com>
2019-08-12 11:17:22 +0100

    added a controller method

---
+++ SpringFramework5/springframework.intro.springboot.jokes.app/src/main/java/springframework/intro/springboot/jokes/app/controllers/JokeController.java
@@ -0,0 +1,28 @@
+package springframework.intro.springboot.jokes.app.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import springframework.intro.springboot.jokes.app.services.JokeService;
+
+@Controller
+public class JokeController {
+
+	private JokeService jokeService;
+
+	@Autowired
+	public JokeController(JokeService jokeService) {
+		super();
+		this.jokeService = jokeService;
+	}
+	
+	@GetMapping("/")
+	public String showJoke(Model model) {
+		
+		model.addAttribute("joke", jokeService.getJoke());
+		return "chucknorris"; 
+	}
+	
+}

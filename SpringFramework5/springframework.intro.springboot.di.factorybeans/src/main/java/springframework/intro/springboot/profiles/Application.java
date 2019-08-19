281f373dedd1dc4843717618d0d8ce52c71f21f4
nelson <srandrediaz@gmail.com>
2019-08-19 12:27:02 +0100

    clean code

--- SpringFramework5/springframework.intro.springboot.di.factorybeans/src/main/java/springframework/intro/springboot/profiles/Application.java
+++ SpringFramework5/springframework.intro.springboot.di.factorybeans/src/main/java/springframework/intro/springboot/profiles/Application.java
@@ -7,10 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-import springframework.intro.springboot.profiles.controllers.ConstructorInjectedController;
 import springframework.intro.springboot.profiles.controllers.MyController;
-import springframework.intro.springboot.profiles.controllers.PropertyInjectedController;
-import springframework.intro.springboot.profiles.controllers.SetterInjectedController;
 
 @SpringBootApplication
 public class Application {
@@ -23,17 +20,9 @@ public class Application {
 		
 		//get Bean "MyController" by type
 		MyController myController = appContext.getBean(MyController.class);
-		//get Bean "PropertyInjectedController" by Bean type
-		PropertyInjectedController propertyInjectedController = appContext.getBean(PropertyInjectedController.class);
-		//get Bean "SetterInjectedController" by Bean Id and Bean type 
-		SetterInjectedController setterInjectedController = appContext.getBean("setterInjectedController", SetterInjectedController.class);
-		//get Bean "ConstructorInjectedController" by Bean type
-		ConstructorInjectedController constructorInjectedController = appContext.getBean(ConstructorInjectedController.class);
 		
 		LOGGER.info("{} Bean is -> " + myController.hello());
-		LOGGER.info("{} PropertyInjectedController Bean is -> " + propertyInjectedController.sayHello());
-		LOGGER.info("{} SetterInjectedController Bean is -> " + setterInjectedController.sayHello());
-		LOGGER.info("{} ConstructorInjectedController Bean is -> " + constructorInjectedController.sayHello());
+		
 	}
 
 }

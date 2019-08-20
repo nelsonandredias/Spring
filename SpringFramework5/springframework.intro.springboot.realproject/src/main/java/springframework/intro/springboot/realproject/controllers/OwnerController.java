c9d7ff94c7a879fc7f59c8ed7e61e3380ccb9d03
nelson <srandrediaz@gmail.com>
2019-08-20 11:55:22 +0100

    list owners in thymeleaf html page

--- SpringFramework5/springframework.intro.springboot.realproject/src/main/java/springframework/intro/springboot/realproject/controllers/OwnerController.java
+++ SpringFramework5/springframework.intro.springboot.realproject/src/main/java/springframework/intro/springboot/realproject/controllers/OwnerController.java
@@ -1,14 +1,29 @@
 package springframework.intro.springboot.realproject.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import springframework.intro.springboot.realproject.services.OwnerService;
+
 @Controller
 public class OwnerController {
 
+	private final OwnerService ownerService;
+	
+	@Autowired
+	public OwnerController(OwnerService ownerService) {
+		super();
+		this.ownerService = ownerService;
+	}
+
+	
+
 	@GetMapping("/owners")
 	public String listOwners(Model model) {
+		
+		model.addAttribute("owners", ownerService.findAll());
 		
 		return "owners/owner";
 	}

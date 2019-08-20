965713b2fc348d4762c82b5f3893972e6a7dd566
nelson <srandrediaz@gmail.com>
2019-08-20 11:59:20 +0100

    list vets in thymeleaf html page

--- SpringFramework5/springframework.intro.springboot.realproject/src/main/java/springframework/intro/springboot/realproject/controllers/VeterinaryController.java
+++ SpringFramework5/springframework.intro.springboot.realproject/src/main/java/springframework/intro/springboot/realproject/controllers/VeterinaryController.java
@@ -1,14 +1,31 @@
 package springframework.intro.springboot.realproject.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import springframework.intro.springboot.realproject.services.VeterinaryService;
+
 @Controller
 public class VeterinaryController {
 
+	
+	private final VeterinaryService veterinaryService;
+	
+	
+	@Autowired
+	public VeterinaryController(VeterinaryService veterinaryService) {
+		super();
+		this.veterinaryService = veterinaryService;
+	}
+
+
+
 	@GetMapping("/vets")
 	public String listVets(Model model) {
+		
+		model.addAttribute("vets", veterinaryService.findAll());
 		
 		return "vets/veterinary";
 	}

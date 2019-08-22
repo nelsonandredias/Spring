834360fb5c1e1e7b1230530aa0e9a6f03e014a1f
nelson <srandrediaz@gmail.com>
2019-08-22 12:16:48 +0100

    started new mvc project

---
+++ SpringFramework5/springframework.intro.springboot.mvc.recipes/src/main/java/springframework/intro/springboot/mvc/recipes/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package springframework.intro.springboot.mvc.recipes.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+	
+	@GetMapping({"", "/index", "/index"})
+	public String getIndexPage() {
+		
+		return "index";
+	}
+	
+}

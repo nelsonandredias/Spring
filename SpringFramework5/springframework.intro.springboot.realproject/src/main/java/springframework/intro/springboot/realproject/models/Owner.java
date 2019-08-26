f415a7405c3bad468e013639ba8c86ab13457d8f
nelson <srandrediaz@gmail.com>
2019-08-26 11:48:36 +0100

    added a visit model class and updated the owner model class

--- SpringFramework5/springframework.intro.springboot.realproject/src/main/java/springframework/intro/springboot/realproject/models/Owner.java
+++ SpringFramework5/springframework.intro.springboot.realproject/src/main/java/springframework/intro/springboot/realproject/models/Owner.java
@@ -1,5 +1,20 @@
 package springframework.intro.springboot.realproject.models;
 
+import java.util.Set;
+
 public class Owner extends Person {
 
+	private Set<Pet> pets;
+
+	
+	public Set<Pet> getPets() {
+		return pets;
+	}
+
+	public void setPets(Set<Pet> pets) {
+		this.pets = pets;
+	}
+	
+	
+	
 }

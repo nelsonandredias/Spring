97dad318457ad1fda03b8fc205ac96d804df6978
nelson <srandrediaz@gmail.com>
2019-08-28 10:49:52 +0100

    added another one to one relationship

--- SpringFramework5/springframework.intro.springboot.jparelations.recipes/src/main/java/springframework/intro/springboot/jparelations/recipes/domains/Ingredient.java
+++ SpringFramework5/springframework.intro.springboot.jparelations.recipes/src/main/java/springframework/intro/springboot/jparelations/recipes/domains/Ingredient.java
@@ -2,11 +2,15 @@ package springframework.intro.springboot.jparelations.recipes.domains;
 
 import java.math.BigDecimal;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
 
 @Entity
 public class Ingredient {
@@ -21,6 +25,10 @@ public class Ingredient {
 	@ManyToOne
 	private Recipe recipe;
 
+	@OneToOne(fetch = FetchType.EAGER)
+	private UnitOfMeasure unitOfMeasure;
+
+	
 	public Long getId() {
 		return id;
 	}
@@ -52,6 +60,16 @@ public class Ingredient {
 	public void setRecipe(Recipe recipe) {
 		this.recipe = recipe;
 	}
+
+	public UnitOfMeasure getUnitOfMeasure() {
+		return unitOfMeasure;
+	}
+
+	public void setUnitOfMeasure(UnitOfMeasure unitOfMeasure) {
+		this.unitOfMeasure = unitOfMeasure;
+	}
+	
+	
 	
 	
 }
---
+++ SpringFramework5/springframework.intro.springboot.jparelations.recipes/src/main/java/springframework/intro/springboot/jparelations/recipes/domains/UnitOfMeasure.java
@@ -0,0 +1,45 @@
+package springframework.intro.springboot.jparelations.recipes.domains;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+public class UnitOfMeasure {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String uom;
+	
+	@OneToOne
+	private Ingredient igredient;
+
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUom() {
+		return uom;
+	}
+
+	public void setUom(String uom) {
+		this.uom = uom;
+	}
+
+	public Ingredient getIgredient() {
+		return igredient;
+	}
+
+	public void setIgredient(Ingredient igredient) {
+		this.igredient = igredient;
+	}
+	
+	
+	
+}

58d87d4a2779eb2cff3cb8dd6e4dc1c070a73fab
nelson <srandrediaz@gmail.com>
2019-08-29 15:44:54 +0100

    set BaseEntity class as a @MappedSuperclass

--- SpringFramework5/springframework.intro.springboot.realproject/src/main/java/springframework/intro/springboot/realproject/models/BaseEntity.java
+++ SpringFramework5/springframework.intro.springboot.realproject/src/main/java/springframework/intro/springboot/realproject/models/BaseEntity.java
@@ -2,8 +2,21 @@ package springframework.intro.springboot.realproject.models;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+// Any class declared as superclass with the interface @MappedSuperclass cannot be an entity
+/*
+ * @MappedSuperclass tells the JPA provider to include the base class persistent properties as 
+ 	if they were declared by the child class extending the superclass annotated with @MappedSuperclass.
+ */
+@MappedSuperclass
 public class BaseEntity implements Serializable{
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId() {
@@ -14,6 +27,8 @@ public class BaseEntity implements Serializable{
 		this.id = id;
 	}
 	
-	
+	public boolean isNew() {
+		return this.id == null;
+	}
 	
 }

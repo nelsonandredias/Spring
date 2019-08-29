908a07e63123f813eedc9b6036ed1ac49ce2d285
nelson <srandrediaz@gmail.com>
2019-08-29 15:49:15 +0100

    set Person class as a @MappedSuperclass

--- SpringFramework5/springframework.intro.springboot.realproject/src/main/java/springframework/intro/springboot/realproject/models/Person.java
+++ SpringFramework5/springframework.intro.springboot.realproject/src/main/java/springframework/intro/springboot/realproject/models/Person.java
@@ -1,10 +1,20 @@
 package springframework.intro.springboot.realproject.models;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+//Any class declared as superclass with the interface @MappedSuperclass cannot be an entity
+/*
+* @MappedSuperclass tells the JPA provider to include the base class persistent properties as 
+	if they were declared by the child class extending the superclass annotated with @MappedSuperclass.
+*/
+@MappedSuperclass
 public class Person extends BaseEntity {
 	
+	@Column(name = "first_name")
 	private String firstName;
+	@Column(name = "last_name")
 	private String lastName;
-	
 	
 	
 	public String getFirstName() {
